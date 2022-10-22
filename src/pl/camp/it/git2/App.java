d6bc508892f12a6ba579c068c20b1d3e3c7cf2da
tkrystian <t.krystian@gmail.com>
2022-10-22 14:11:24 +0200

    nowy branch

--- src/pl/camp/it/git2/App.java
+++ src/pl/camp/it/git2/App.java
@@ -3,5 +3,6 @@ package pl.camp.it.git2;
 public class App {
     public static void main(String[] args) {
         System.out.println("GIT2");
+        System.out.println("nowy branch commit2");
     }
 }
