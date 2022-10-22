d8433cab0af9bc28f2f21680a7b1e8f7a14bfebe
tkrystian <t.krystian@gmail.com>
2022-10-22 14:16:21 +0200

    cos-tam-2

--- src/pl/camp/it/git2/App.java
+++ src/pl/camp/it/git2/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("GIT2");
         System.out.println("nowy branch commit2");
-
+        System.out.println("cos-tam-2 1");
     }
 }
